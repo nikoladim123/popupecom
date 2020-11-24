17b19946d3f6cbc11b0ac267943476413d0eeddb
unknown <nikoladimitrijevic1991@gmail.com>
2020-11-25 00:52:05 +0100

    asd

--- assets/js/services-popup.js
+++ assets/js/services-popup.js
@@ -114,7 +114,6 @@ function dotslideChange(){
     });
 
 
-
     return rectWorkImageBoxPosition,recentWorkStage;
 
   });
@@ -123,3 +122,101 @@ function dotslideChange(){
 
 }
 dotslideChange()
+
+
+// whatWeDo Section carousel mobile
+var fourImgGrid = document.querySelector('.promiseGrid');
+var servicesDot = document.getElementsByClassName('servicesDot');
+
+fourImgGrid.addEventListener('touchstart', dragStartServices);
+fourImgGrid.addEventListener('touchmove', dragMoveServices);
+fourImgGrid.addEventListener('touchend', endPositonServices);
+
+var servicesWorkImageBoxPosition = 0;
+var servicesWorkStage = 0;
+var curentMousePosServices = 0;
+var startPosServices;
+
+function dragStartServices(e) {
+  if(fourImgGrid.style.left =='-300%'||fourImgGrid.style.left=="-100%"||fourImgGrid.style.left== "-200%"||fourImgGrid.style.left== "0%"){
+
+  }else{
+  startPosServices = e.touches[0].clientX +(servicesWorkStage*window.innerWidth);
+    // stop transition
+  fourImgGrid.style.transition = '0s';
+  }
+}
+
+function dragMoveServices(e) {
+  fourImgGrid.style.left = e.touches[0].clientX - startPosServices + 'px';
+}
+
+function endPositonServices(e) {
+  fourImgGrid.style.transition = '.5s ease-in';
+    // position logic
+console.log(fourImgGrid.style.left);
+if(fourImgGrid.style.left =='-300%'||fourImgGrid.style.left=="-100%"||fourImgGrid.style.left== "-200%"||fourImgGrid.style.left== "0%"){
+
+}else{
+  dotsChangeServices()
+  if(e.changedTouches[0].clientX < window.innerWidth/3 && servicesWorkStage < 5){
+    servicesWorkImageBoxPosition -=100;
+    servicesWorkStage+=1;
+  }else if(e.changedTouches[0].clientX > window.innerWidth/2 && servicesWorkStage > 0){
+    servicesWorkImageBoxPosition +=100;
+    servicesWorkStage-=1;
+  }
+
+}
+  fourImgGrid.style.left = servicesWorkImageBoxPosition + '%';
+
+  dotColorChangeServices()
+}
+
+function dotColorChangeServices() {
+  for (var i = 0; i < servicesDot.length-1; i++) {
+    servicesDot[i].style.background = 'transparent';
+  }
+  servicesDot[servicesWorkStage].style.background = 'white';
+}
+
+
+
+function dotsChangeServices(){
+  let selectDots = document.querySelectorAll('.servicesDotsContainer .servicesDot ')
+  let imAdots = Array.from(selectDots);
+  let slidesImg = Array.from(document.querySelectorAll(".promiseContainer .promiseGridBox"))
+
+  imAdots.forEach(eachDots => {
+
+    eachDots.addEventListener("click",function(){
+      //Change Slides
+
+      getCurrentIndex= imAdots.indexOf(this);
+      selectedSlidesImg = slidesImg[getCurrentIndex]
+      fourImgGrid.style.left = (getCurrentIndex*-1 + '00%');
+
+    (getCurrentIndex==0?servicesWorkImageBoxPosition=0:
+      servicesWorkImageBoxPosition = (getCurrentIndex*-100))
+
+    if(slidesImg.indexOf(selectedSlidesImg)==0){
+      servicesWorkStage=0
+    }else{
+      servicesWorkStage = slidesImg.indexOf(selectedSlidesImg)
+    }
+
+    this.style.background="#fff"
+    const filteredItemsImg = imAdots.filter((value, index) => getCurrentIndex !== index);
+    filteredItemsImg.forEach(dot=>{
+      dot.style.background="transparent"
+    })
+    });
+
+
+
+    return servicesWorkImageBoxPosition,servicesWorkStage;
+
+  });
+
+}
+dotsChangeServices()
